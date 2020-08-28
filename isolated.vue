ba978658b10e2fc13b56284ea2dfccb556ac4639
Alex Varjabedian <alex@trueandco.com>
2020-08-28 16:06:18 -0700

    run a independant server for a single file (isolated component) for testing/prototyping a component, command: vue serve isolated.vue

---
+++ isolated.vue
@@ -0,0 +1,5 @@
+<template>
+	<h1>This file could be served independantly to prototype using: vue serve isolated.vue</h1>
+	<p>Note that you need to be using vue-cli-3 and have: npm install -g @vue/cli-service-global</p>
+	<p>Also note that since this file is in a directory which has a package.json file it will not work, it needs to be completely isolated</p>
+</template>
