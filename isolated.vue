e9d67be263dca07eb1de901fee5ed336fb8dde45
Alex Varjabedian <alex@trueandco.com>
2020-08-29 00:04:20 -0700

    vue basics

--- isolated.vue
+++ isolated.vue
@@ -1,5 +1,24 @@
 <template>
-	<h1>This file could be served independantly to prototype using: vue serve isolated.vue</h1>
-	<p>Note that you need to be using vue-cli-3 and have: npm install -g @vue/cli-service-global</p>
-	<p>Also note that since this file is in a directory which has a package.json file it will not work, it needs to be completely isolated</p>
+	<div id="only-one-main-element">
+		<h1>This file could be served independantly to prototype using: vue serve isolated.vue</h1>
+		<p>Note that you need to be using vue-cli-3 and have: npm install -g @vue/cli-service-global</p>
+		<p>Also note that since this file is in a directory which has a package.json file it will not work, it needs to be completely isolated</p>
+
+		<div v-if="online">
+			<p>A staff member is currently online</p>
+		</div>
+		<div v-if="!online">
+			<p>A staff member is not currently online</p>
+		</div>
+	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				online: false
+			}
+		}
+	}
+</script>
